42d86bd76b4d27e3ec4382440dfe46d42a450144
akerrules <addrido@gmail.com>
2024-02-02 11:09:49 -0500

    added images

--- products.js
+++ products.js
@@ -1,32 +1,88 @@
 // Categories to the products pages: vegetables, fruits, dairy, meat, fish, bread
 // options: organic, glutenFree, lactoseFree
 
+// prettier-ignore
 var products = [
-    {name: "Carrot", value: "carrot", price: 0.5, fruits: true, vegetables: true, organic: true, glutenFree: true, lactoseFree: true},
-    {name: "Apple", value: "apple", price: 1, fruits: true, organic: true, glutenFree: true, lactoseFree: true},
-    {name: "Milk", value: "milk", price: 2, dairy: true, organic: false, glutenFree: true, lactoseFree: false},
-    {name: "Beef", value: "beef", price: 5, meat: true, organic: false, glutenFree: true, lactoseFree: true},
-    {name: "Salmon", value: "salmon", price: 7, fish: true, organic: false, glutenFree: true, lactoseFree: true},
-    {name: "Bread", value: "bread", price: 3, bread: true, organic: true, glutenFree: false, lactoseFree: true}
+  {
+    name: "Carrot",
+    value: "carrot",
+    price: 0.5,
+    fruits: true,
+    vegetables: true,
+    organic: true,
+    glutenFree: true,
+    lactoseFree: true,
+    img: "/images/carrot.jpg",
+  },
+  {
+    name: "Apple",
+    value: "apple",
+    price: 1,
+    fruits: true,
+    organic: true,
+    glutenFree: true,
+    lactoseFree: true,
+    img: "/images/apple.jpg",
+  },
+  {
+    name: "Milk",
+    value: "milk",
+    price: 2,
+    dairy: true,
+    organic: false,
+    glutenFree: true,
+    lactoseFree: false,
+    img: "/images/carrot.jpg",
+  },
+  {
+    name: "Beef",
+    value: "beef",
+    price: 5,
+    meat: true,
+    organic: false,
+    glutenFree: true,
+    lactoseFree: true,
+    img: "/images/beef.jpg",
+  },
+  {
+    name: "Salmon",
+    value: "salmon",
+    price: 7,
+    fish: true,
+    organic: false,
+    glutenFree: true,
+    lactoseFree: true,
+    img: "/images/salmon.jpg",
+  },
+  {
+    name: "Bread",
+    value: "bread",
+    price: 3,
+    bread: true,
+    organic: true,
+    glutenFree: false,
+    lactoseFree: true,
+    img: "/images/bread.jpg",
+  },
 ];
-function getProducts(type){
-    var filteredProducts = [];
-    for(var i = 0; i < products.length; i++){
-        if(products[i][type] == true){
-            filteredProducts.push(products[i]);
-        }
+function getProducts(type) {
+  var filteredProducts = [];
+  for (var i = 0; i < products.length; i++) {
+    if (products[i][type] == true) {
+      filteredProducts.push(products[i]);
     }
-    return filteredProducts;
+  }
+  return filteredProducts;
 }
 
 function getTotal(elems) {
-    var sum = 0;
-    for(var i = 0; i < elems.length; i++){
-        for(var j = 0; j < products.length; j++){
-            if(elems[i] == products[j].value){
-                sum += products[j].price;
-            }
-        }
+  var sum = 0;
+  for (var i = 0; i < elems.length; i++) {
+    for (var j = 0; j < products.length; j++) {
+      if (elems[i] == products[j].value) {
+        sum += products[j].price;
+      }
     }
-    return sum;
+  }
+  return sum;
 }
--- script.js
+++ script.js
@@ -1,91 +1,101 @@
 function openSection(event, sectionName) {
-    var i, sections, tablinks;
-    // Go over every child of the element with id="sections" and hide it
-    sections = document.getElementById("sections");
-    for (i = 0; i < sections.children.length; i++) {
-        sections.children[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(sectionName).style.display = "block";
-    event.currentTarget.parentElement.className += " active";
+  var i, sections, tablinks;
+  // Go over every child of the element with id="sections" and hide it
+  sections = document.getElementById("sections");
+  for (i = 0; i < sections.children.length; i++) {
+    sections.children[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(sectionName).style.display = "block";
+  event.currentTarget.parentElement.className += " active";
 }
 
-function populateProducts(){
-    var productCategory = document.getElementById("category");
-    // Get the element selected from radio buttons
-    var checked = productCategory.querySelectorAll('input[type=radio]:checked');
-    checked = checked[0];
-    var productsList = document.getElementById("listProducts");
+function populateProducts() {
+  var productCategory = document.getElementById("category");
+  // Get the element selected from radio buttons
+  var checked = productCategory.querySelectorAll("input[type=radio]:checked");
+  checked = checked[0];
+  var productsList = document.getElementById("listProducts");
 
-    // Remove all products
-    while (productsList.firstChild) {
-        productsList.removeChild(productsList.firstChild);
-    }
+  // Remove all products
+  while (productsList.firstChild) {
+    productsList.removeChild(productsList.firstChild);
+  }
 
-    if (checked.length == 0) {
-        return;
-    }
+  if (checked.length == 0) {
+    return;
+  }
 
-    // Add products
-    var ul = document.createElement("ul");
-    var productsToAdd = [];
-    productsToAdd.push.apply(productsToAdd, getProducts(checked.value));
+  // Add products
+  var ul = document.createElement("ul");
+  var productsToAdd = [];
+  productsToAdd.push.apply(productsToAdd, getProducts(checked.value));
 
+  // Filter products by price
+  productsToAdd.sort(compareProducts);
+  // remove duplicates
+  productsToAdd = productsToAdd.filter(
+    (product, index, self) =>
+      self.findIndex((p) => p.name === product.name) === index
+  );
 
-    // Filter products by price
-    productsToAdd.sort(compareProducts);
-    // remove duplicates
-    productsToAdd = productsToAdd
-    .filter((product, index, self) => self.findIndex(p => p.name === product.name) === index);
+  for (j = 0; j < productsToAdd.length; j++) {
+    var li = document.createElement("li");
+    var product = productsToAdd[j];
+    var input = document.createElement("input");
+    var img = document.createElement("img");
+    img.src = product.img;
+    img.height = 200;
+    img.width = 200;
+    img.style.padding = "10px";
+    input.type = "checkbox";
+    input.className = "product";
+    input.value = product.value;
+    input.name = product.name;
+    li.appendChild(img);
+    li.appendChild(input);
 
-    for (j = 0; j < productsToAdd.length; j++) {
-        var li = document.createElement("li");
-        var product = productsToAdd[j];
-        var input = document.createElement("input");
-        input.type = "checkbox";
-        input.className = "product";
-        input.value = product.value;
-        input.name = product.name;
-        li.appendChild(input);
-        li.appendChild(document.createTextNode(product.name + " - " + product.price + "$"));
-        ul.appendChild(li);
-    }
-    
-    productsList.appendChild(ul);
+    li.appendChild(
+      document.createTextNode(product.name + " - " + product.price + "$")
+    );
+    ul.appendChild(li);
+  }
+
+  productsList.appendChild(ul);
 }
 
-function compareProducts(a, b){
-    return a.price - b.price;
+function compareProducts(a, b) {
+  return a.price - b.price;
 }
 function addToCart() {
-    var cart = document.getElementById("cart");
-    var products = document.getElementsByClassName("product");
-    var cartsElements = [];
+  var cart = document.getElementById("cart");
+  var products = document.getElementsByClassName("product");
+  var cartsElements = [];
 
-    while (cart.firstChild) {
-        cart.removeChild(cart.firstChild);
+  while (cart.firstChild) {
+    cart.removeChild(cart.firstChild);
+  }
+
+  var text = document.createElement("p");
+  text.innerHTML = "Cart:";
+  text.appendChild(document.createElement("br"));
+  for (i = 0; i < products.length; i++) {
+    if (products[i].checked) {
+      cartsElements.push(products[i].value);
+      text.appendChild(document.createTextNode(products[i].value));
+      text.appendChild(document.createElement("br"));
     }
-    
-    var text = document.createElement("p");
-    text.innerHTML = "Cart:";
-    text.appendChild(document.createElement("br"));
-    for (i = 0; i < products.length; i++) {
-        if (products[i].checked) {
-            cartsElements.push(products[i].value);
-            text.appendChild(document.createTextNode(products[i].value));
-            text.appendChild(document.createElement("br"));
-        }
-    }
-    if (cartsElements.length == 0) {
-        text.appendChild(document.createTextNode("Cart is empty"));
-        cart.appendChild(text);
-    } else {
-        cart.appendChild(text);
-        cart.appendChild(document.createTextNode("Total: " + getTotal(cartsElements) + "$"));
-    }
-    
+  }
+  if (cartsElements.length == 0) {
+    text.appendChild(document.createTextNode("Cart is empty"));
+    cart.appendChild(text);
+  } else {
+    cart.appendChild(text);
+    cart.appendChild(
+      document.createTextNode("Total: " + getTotal(cartsElements) + "$")
+    );
+  }
 }
-
